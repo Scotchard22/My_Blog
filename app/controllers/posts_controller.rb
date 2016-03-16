ca1092126f1957815256e0999de9dd83671489c8
Scotchard22 <scotchard@aol.com>
2016-03-16 16:13:46 -0400

    added edit and delete methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
   
   def new
-
+    @post = Post.new
   end
 
   def show
@@ -14,18 +14,33 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.save
 
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
   
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title,:body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end  
 
   private
